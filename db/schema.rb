719d8137822be9d7eeeedaf36aa7546b66f2cdc0
Dominika <dominikasiek@gmail.com>
2019-02-20 12:55:10 +0100

    Add welcome controller and change routes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_133936) do
+ActiveRecord::Schema.define(version: 2019_02_20_084703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
